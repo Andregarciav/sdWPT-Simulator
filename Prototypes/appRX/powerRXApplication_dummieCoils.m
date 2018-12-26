12f542eb2f194c56ea33729453818e174232c421
andregarciav <andregarcia@dcc.ufmg.br>
2018-12-26 20:15:25 -0200

    Update powerRXApplication_dummieCoils.m

--- Prototypes/appRX/powerRXApplication_dummieCoils.m
+++ Prototypes/appRX/powerRXApplication_dummieCoils.m
@@ -1,8 +1,8 @@
 classdef powerRXApplication_dummieCoils < powerRXApplication
     properties
         interval = 2;
-        One_hope = [];
-        two_hope = [];
+        mpr_ant = [];
+        %two_hope = [];
         g = graph
         wantAck = false;
         cont = 0;
@@ -25,14 +25,11 @@ classdef powerRXApplication_dummieCoils < powerRXApplication
         function [obj,netManager,WPTManager] = handleMessage(obj,data,GlobalTime,netManager,WPTManager)
             data;
             msgType = str2num(data(1)); %tipo de msg recebida
-            msg_len = data(2);
+            msg_len = str2num(data(2));
             src = str2num(data(3));
             
             if msgType == 0
-                if isempty(obj.One_hope(obj.One_hope == src))
-                    obj.One_hope = [obj.One_hope src];
-                end
-                %%%%%%%%%%%%%%%%%%%%%% ADD NODES AND EDGES IN GRAPH %%%%%%%%%%
+                
                 v = neighbors(obj.g, string(obj.ID));
                 if isempty(v)
                     obj.g = addnode(obj.g, string(src));
@@ -43,39 +40,10 @@ classdef powerRXApplication_dummieCoils < powerRXApplication
                     obj.g = addnode(obj.g, string(src));
                     obj.g = addedge(obj.g, string(obj.ID), string(src));
                 end
-                    %%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%
-
-
-                obj.payload = ['1',msg_len,string(obj.ID),string(obj.One_hope)];
-                obj = setSendOptions(obj, 0, 25000,5);
-                netManager = send(obj, netManager, src, obj.payload,length(obj.payload)*32, GlobalTime);
-            
-            elseif msgType == 1
-
-                if isempty(obj.One_hope(obj.One_hope == src))
-                    if isempty(obj.two_hope(obj.two_hope == src)) == 0
-                        obj.two_hope(obj.two_hope == src) = [];
-                    end
-                    obj.One_hope = [obj.One_hope src];
-                end
                 
-                 %%%%%%%%%%%%%%%%%%%%%% ADD NODES AND EDGES IN GRAPH %%%%%%%%%%
-                
-                 if (findnode(obj.g, string(src))==0)
-                     obj.g = addnode(obj.g, string(src));
-                     obj.g = addedge(obj.g, string(obj.ID), string(src));
-                 end
-                 %%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%
-
-                 if length(data) > 3
+                if length(data) > 3
                     for r = 4:(length(data))
                         temp = str2num(data(r));
-                        if (isempty(obj.One_hope(obj.One_hope==temp))) && (obj.ID~=temp) % se obj não existir na lista de um salto e for vizinho
-                            if isempty(obj.two_hope(obj.two_hope==temp))
-                                obj.two_hope = [obj.two_hope temp];
-                            end
-                        end
-                        %%%%%%%%%%%%%%%%%%%%%% ADD NODES AND EDGES IN GRAPH %%%%%%%%%%
                         if (findnode(obj.g, string(temp))==0)
                             obj.g = addnode(obj.g, string(temp));
                             obj.g = addedge(obj.g, string(src), string(temp));
@@ -83,16 +51,58 @@ classdef powerRXApplication_dummieCoils < powerRXApplication
                         if (findedge(obj.g, string(src), string(temp)) == 0)
                             obj.g = addedge(obj.g, string(src), string(temp));
                         end
-                        %%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%
                     end
                 end
-                obj.wantAck = false;
-                obj.cont = 0;
+
+
+                %obj.lmpr = mpr(obj,obj.lmpr);
+                %obj.payload = constructPayload (obj,0,0,0,0,0);
+                %obj = setSendOptions(obj, 0, 25000,5);
+                %netManager = send(obj, netManager, src, obj.payload,length(obj.payload)*32, GlobalTime);
+            
+            elseif msgType == 1
+                if ~(isempty(obj.mpr_ant == src)) && (str2num(data(6) > 1))
+                    origem = str2num(data(4));
+                    dst = str2num(data(5));
+                    ttl = str2num(data(6)) - 1;
+                    for r=1:6
+                        data(r) = [];
+                    end
+                    if (findnode(obj.g, data(5)) == 0) %se eu não conheço reencaminha a msg
+                        obj.payload = constructPayload (obj,1,origem,dst,ttl,data);
+                    elseif (findnode(obj.g, data(5)) ~= 0)
+                        obj.payload = constructPayload (obj,3,origem,dst,ttl,data);
+                    end
+                        obj = setSendOptions(obj, 0, 25000,5);
+                        netManager = broadcast(obj,netManager,obj.payload,length(obj.payload)*32,GlobalTime);
+                end
+
             elseif msgType == 2
                 
-            end
-            for r = 1:length(obj.One_hope)
-                obj.two_hope(obj.two_hope == obj.One_hope(r)) = [];
+                if ~(isempty(strcmp(data, string(obj.ID))))
+                    if isempty(obj.mpr_ant == src)
+                        obj.mpr_ant = [obj.mpr_ant src];
+                    end
+                end
+
+            elseif msgType == 3
+                origem = str2num(data(4));
+                dst = str2num(data(5));
+                
+                for r=1:5
+                    data(r) = [];
+                end
+                
+                if str2num(dst == obj.ID)
+                    disp('MSG RECEBIDA')
+                    disp(['Origem :', origem])
+                    disp(data)
+                elseif ~(isempty(obj.mpr_ant == src)) && findnode(obj.g, string(src) ~= 0)
+                    obj.payload = constructPayload (obj,3,origem,dst,0,data);
+                    obj = setSendOptions(obj, 0, 25000,5);
+                    netManager = broadcast(obj,netManager,obj.payload,length(obj.payload)*32,GlobalTime);
+                end
+
             end
 
             obj.APPLICATION_LOG.DATA = obj;
@@ -105,45 +115,23 @@ classdef powerRXApplication_dummieCoils < powerRXApplication
 
         function [obj,netManager,WPTManager] = handleTimer(obj,GlobalTime,netManager,WPTManager)
 
+            
+            
             v = neighbors(obj.g, string(obj.ID)); %Verifica se exite vizinho
             
+            obj.payload = constructPayload(obj,0,0,0,0,0);
+            obj = setSendOptions(obj, 0, 25000,5);
+            netManager = broadcast(obj,netManager,obj.payload,length(obj.payload)*32,GlobalTime);
+
             obj.lmpr = mpr(obj);
-
-            if (obj.wantAck == false) && isempty(v) %caso não tenha vizinho
-                obj.payload = ['0','0',string(obj.ID)];
-                payloadLen = length(obj.payload)*32;
-                %canal 0 de (vlc sobre) swipt, 1000bps, 5W
+        
+            if (isempty(obj.lmpr) == 0)
+                obj.payload = constructPayload(obj,2,0,0,0,0);
                 obj = setSendOptions(obj, 0, 25000,5);
-                netManager = broadcast(obj,netManager,obj.payload,payloadLen,GlobalTime);
-                obj.wantAck = true;
-
-            else
-                if obj.cont < 2
-                    netManager = broadcast(obj,netManager,obj.payload,length(obj.payload),GlobalTime);
-                    obj.cont = obj.cont + 1;
-                else
-                    obj.cont = 0;
-                    obj.wantAck = false;
-                    obj.listControl = obj.listControl - 1;
-                    if obj.listControl == 0
-                        obj.g = graph;
-                        obj.g = addnode(obj.g, string(obj.ID));
-                        obj.One_hope = [];
-                        obj.two_hope = [];
-                        obj.listControl = 4;
-                    end
-                end
+                netManager = broadcast(obj,netManager,obj.payload,length(obj.payload)*32,GlobalTime);
             end
             
-            peso = [];
-            for r = 1:length(v)
-                peso = [peso 4];
-            end
             
-            for r = 1:length(v)
-                obj.payload = ['2','0',string(obj.ID)];
-                netManager = send(obj, netManager, str2num(v(r)), obj.payload,length(obj.payload)*32, GlobalTime);
-            end
 
             %figure(obj.ID);
             %    plot (obj.g);
